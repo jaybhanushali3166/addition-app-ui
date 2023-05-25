5245cbbee283b569a83194db4d10d706c5343f56
Jay Bhanushali <jaybhanushali3166@gmail.com>
2023-05-25 14:32:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const AddNumbers = () => {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [sum, setSum] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:80/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        num1: Number(number1),
+        num2: Number(number2),
+      }),
+    });
+
+    const data = await response.json();
+    const result = data.results.sum;
+    setSum(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Number 1:
+          <input
+            type="number"
+            value={number1}
+            onChange={(e) => setNumber1(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Number 2:
+          <input
+            type="number"
+            value={number2}
+            onChange={(e) => setNumber2(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {sum && <p>Sum: {sum}</p>}
     </div>
   );
-}
+};
 
-export default App;
+export default AddNumbers;
